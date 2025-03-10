ee81c58009bbf48704361a876b67aca2bd6c22af
Kamol <kamolsermatov096@gmail.com>
2025-03-10 13:06:49 +0500

    Initial commit

---
+++ CompleteMethodAdd/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace CompleteMethodAdd;
+
+public class Program
+{
+    public static void Main()
+    {
+        
+    }
+    public static int Add(int a, int b)
+    {
+        throw new NotImplementedException();
+    }
+}
